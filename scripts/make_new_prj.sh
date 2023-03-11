66e365c12b766a130aa7637e44dcc59ad6612551
PulseRainmaker <PulseRainmaker@users.noreply.github.com>
2023-03-11 01:14:29 -0800

    update make_new_prj.sh to check repo_root and repo_name

--- scripts/make_new_prj.sh
+++ scripts/make_new_prj.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+##################################################################################################
+## script to make a new project folder based on the HelloWorld template
+##################################################################################################
+
 if [ "$#" -lt 1 ]; then
   echo "==> Please provide the project folder"
   exit 1
@@ -8,11 +12,41 @@ fi
 
 mkdir -p $1;dir_path=`echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"`
 
-repo_root=`git rev-parse --show-toplevel`
+##################################################################################################
+## get script full path
+##################################################################################################
+script_dir=`dirname "$0"`
+script_full_dir=`echo "$(cd "$(dirname "$script_dir")"; pwd)/$(basename "$script_dir")"`
 
+
+##################################################################################################
+## get repo root
+##################################################################################################
+cd $script_full_dir;repo_root=`git rev-parse --show-toplevel`
 echo " "
 echo "===> Repo-Root = $repo_root"
 echo " "
+
+if [ -z "$repo_root" ]; then
+  echo "Please run the script from FpgaLimerick repo clone"
+  exit 1
+fi
+
+##################################################################################################
+## get repo name
+##################################################################################################
+
+cd $script_full_dir;repo_name=`basename -s .git $(git config --get remote.origin.url)`
+
+if [ "$repo_name" != "FpgaLimerick" ]; then
+  echo "Please run the script from FpgaLimerick repo clone"
+  exit 1
+fi
+
+
+##################################################################################################
+## copy files from HelloWorld
+##################################################################################################
 
 set -x
 
@@ -45,5 +79,3 @@ cp -f $repo_root/HelloWorld/test/cocotb/questa/tox.ini $dir_path/test/cocotb/que
 cp -f $repo_root/HelloWorld/test/cocotb/questa/conftest.py $dir_path/test/cocotb/questa
 
 cp -f $repo_root/HelloWorld/test/cocotb/verilator/tox.ini $dir_path/test/cocotb/verilator
-
-
